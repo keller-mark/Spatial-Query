eef36eb8c1f7b7835425cec18b48ee67430eff7c
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2024-05-03 16:07:02 -0400

    Use JSX

--- create-plugins.js
+++ create-plugins.js
@@ -8,7 +8,6 @@ function createPlugins(utilsForPlugins) {
         z,
         useCoordination,
     } = utilsForPlugins;
-    const e = React.createElement;
     function SpatialQueryView(props) {
         const { coordinationScopes } = props;
         const [{
@@ -33,47 +32,48 @@ function createPlugins(utilsForPlugins) {
             setQueryType(e.target.value);
         }, []);
 
-        return e('div', { className: "spatial-query" }, [
-            e('p', {}, 'Spatial Query Manager'),
-            e('label', {}, [
-                'Query type ',
-                e('select', { onChange: onQueryTypeChange }, [
-                    e('option', { value: 'grid' }, 'Grid-based'),
-                    e('option', { value: 'rand' }, 'Random-based'),
-                    e('option', { value: 'ct-center', disabled: cellTypeOfInterest === null }, 'Cell type of interest'),
-                ]),
-            ]),
-            e('br'),
-            e('label', {}, [
-                // Maximum distance to consider a cell as a neighbor.
-                'Max. Dist. ',
-                e('input', { type: 'range', value: maxDist, onChange: e => setMaxDist(parseFloat(e.target.value)), min: 50, max: 250, step: 1 }),
-                maxDist,
-            ]),
-            e('br'),
-            e('label', {}, [
-                // Minimum neighborhood size for each point to consider.
-                'Min. Size ',
-                e('input', { type: 'range', value: minSize, onChange: e => setMinSize(parseFloat(e.target.value)), min: 0, max: 20, step: 1 }),
-                minSize,
-            ]),
-            e('br'),
-            e('label', {}, [
-                // Minimum number of cell type to consider.
-                'Min. Count ',
-                e('input', { type: 'range', value: minCount, onChange: e => setMinCount(parseFloat(e.target.value)), min: 0, max: 30, step: 1 }),
-                minCount,
-            ]),
-            e('br'),
-            e('label', {}, [
-                // Threshold of frequency to consider a pattern as a frequent pattern.
-                'Min. Support ',
-                e('input', { type: 'range', value: minSupport, onChange: e => setMinSupport(parseFloat(e.target.value)), min: 0, max: 1, step: 0.01 }),
-                minSupport,
-            ]),
-            e('br'),
-            // TODO: disDuplicates: Distinguish duplicates in patterns.
-            e('button', { onClick: (e) => {
+        return (
+        <div className="spatial-query">
+            <p>Spatial Query Manager</p>
+            <label>
+                Query type&nbsp;
+                <select onChange={onQueryTypeChange}>
+                    <option value="grid">Grid-based</option>
+                    <option value="rand">Random-based</option>
+                    <option value="ct-center" disabled={cellTypeOfInterest === null}>Cell type of interest</option>
+                </select>
+            </label>
+            <br/>
+            <label>
+                {/* Maximum distance to consider a cell as a neighbor. */}
+                Max. Dist.
+                <input type="range" value={maxDist} onChange={e => setMaxDist(parseFloat(e.target.value))} min={50} max={250} step={1} />
+                {maxDist}
+            </label>
+            <br/>
+            <label>
+                {/* Minimum neighborhood size for each point to consider. */}
+                Min. Size
+                <input type="range" value={minSize} onChange={e => setMinSize(parseFloat(e.target.value))} min={0} max={20} step={1} />
+                {minSize}
+            </label>
+            <br/>
+            <label>
+                {/* Minimum number of cell type to consider. */}
+                Min. Count
+                <input type="range" value={minCount} onChange={e => setMinCount(parseFloat(e.target.value))} min={0} max={30} step={1} />
+                {minCount}
+            </label>
+            <br/>
+            <label>
+                {/* Threshold of frequency to consider a pattern as a frequent pattern. */}
+                Min. Support
+                <input type="range" value={minSupport} onChange={e => setMinSupport(parseFloat(e.target.value))} min={0} max={1} step={0.01} />
+                {minSupport}
+            </label>
+            <br/>
+            {/* TODO: disDuplicates: Distinguish duplicates in patterns. */}
+            <button onClick={(e) => {
                 setQueryParams({
                     cellTypeOfInterest,
                     queryType,
@@ -84,8 +84,9 @@ function createPlugins(utilsForPlugins) {
                     uuid,
                 });
                 setUuid(uuid+1);
-            } }, 'Find patterns'),
-        ]);
+            }}>Find patterns</button>
+        </div>
+        );
     }
 
     const pluginCoordinationTypes = [
